4bf00e9bacb4d74d6b96210fe2a76e1fb5e75563
Unknown <andrew_khang_le@hotmail.com>
2018-06-04 15:43:30 -0500

    added rocket and boost

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+        // thrust while rotating
+        if (Input.GetKey(KeyCode.Space)) {
+            print("boost");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // control directions
+        if (Input.GetKey(KeyCode.A)) {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            print("Rotating right");
+        }
+    }
+}
