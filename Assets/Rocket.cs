bc3db30ec1c12e539205db9e024bb27a9c49b2b8
Unknown <andrew_khang_le@hotmail.com>
2018-06-06 12:38:46 -0500

    fix bugs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,45 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        ProcessInput();
-	}
+    }
 
-    private void ProcessInput() {
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust(){
         // thrust while rotating
-        if (Input.GetKey(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space))
+        {
             //moves up
             print("boost");
             rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying){
+            if (!audioSource.isPlaying)
+            {
                 audioSource.Play();
             }
-        }else{
+        }
+        else
+        {
             audioSource.Stop();
         }
+    }
+    private void Rotate(){
+        rigidBody.freezeRotation = true; //take manual control of rotation
 
         // control directions
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A)){
             //move to the left
             transform.Rotate(Vector3.forward);
             print("Rotating left");
         }
-        else if (Input.GetKey(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D))
+        {
             //move to the right
             transform.Rotate(Vector3.back);
             print("Rotating right");
         }
+        rigidBody.freezeRotation = false; //resume physics control of rotation
     }
 }
+
+ 
